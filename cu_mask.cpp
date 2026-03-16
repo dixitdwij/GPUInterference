99159811438288b23679c9554ce1dd12f752c94d
amusca <anamariamusca1990@gmail.com>
2026-03-16 14:03:08 +0100

    created cu_mask struct

--- cu_mask.cpp
+++ cu_mask.cpp
@@ -5,56 +5,37 @@
 #include "utils.cpp"
 #include "kernels.cpp"
 
+struct cu_mask{
+    uint32_t cuMaskSize;
+    uint32_t cuMask[4];
+    HipStream masked_stream; 
+    const int N;
+    HipVector ha, hb, hc;
+
+    cu_mask(const int n): cuMaskSize(4), cuMask{0x0F, 0, 0, 0}, masked_stream(cuMaskSize, cuMask), N(n), ha(N), hb(N), hc(N, false) {}
+
+    void run_kernel(){
+        ha.toDevice();
+        hb.toDevice();
+
+        int blockSize = 256;
+        int numBlocks = (N + blockSize - 1) / blockSize;
+
+        HipStartStop timer(&masked_stream);
+        timer.startTiming();
+
+        vecAdd<<<numBlocks, blockSize, 0, masked_stream.stream>>>(ha.d_vec, hb.d_vec, hc.d_vec, N);
+        timer.stopTiming();
+
+        float milliseconds = timer.elapsedTime();
+        hc.toHost();
+
+        std::cout << "Execution Time: " << milliseconds << " ms" << std::endl;
+    }
+};
+
 int main() {
-    // Each uint32_t covers 32 CUs. MI210 has 104 CUs (confirm)
-    uint32_t cuMaskSize = 4;
-    uint32_t cuMask[4] = {0, 0, 0, 0};
-    // Only first 4 CUs
-    cuMask[0] = 0x0F; 
-
-    // hipStream_t masked_stream;
-    
-    // Create the stream using the HIP extension for CU masking
-    // HipErrorCheck(hipExtStreamCreateWithCUMask(&masked_stream, cuMaskSize, cuMask));
-    HipStream masked_stream(cuMaskSize, cuMask);
-    std::cout << "Masked stream created." << std::endl;
-
-    const int N = 1 << 30;
-    HipVector ha(N), hb(N), hc(N, false);
-
-    ha.toDevice();
-    hb.toDevice();
-
-    int blockSize = 256;
-    int numBlocks = (N + blockSize - 1) / blockSize;
-
-    std::cout << "Launching vecAdd" << std::endl;
-
-    HipStartStop timer(&masked_stream);
-    timer.startTiming();
-
-    vecAdd<<<numBlocks, blockSize, 0, masked_stream.stream>>>(ha.d_vec, hb.d_vec, hc.d_vec, N);
-
-    // HipErrorCheck(hipStreamSynchronize(masked_stream));
-    timer.stopTiming(); 
-
-    float milliseconds = timer.elapsedTime();
-    
-    hc.toHost();
-
-    // bool correct = true;
-    // for (int i = 0; i < N; i++) {
-    //     if (hc.vec[i] != ha.vec[i] + hb.vec[i]) {
-    //         correct = false;
-    //         break;
-    //     }
-    // }
-
-    // std::cout << "Result: " << (correct ? "PASS" : "FAIL") << std::endl;
-    std::cout << "Execution Time: " << milliseconds << " ms" << std::endl;
-
-    // Cleanup the masked stream
-    // HipErrorCheck(hipStreamDestroy(masked_stream));
-
+    cu_mask program(1 << 30);
+    program.run_kernel();
     return 0;
 }
